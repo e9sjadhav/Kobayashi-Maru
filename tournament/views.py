50f76b109a367cb830de799d533176811c2760b8
e9sjadhav <somraj@enine.dev>
2025-11-03 11:09:38 +0530

    created form, views

--- tournament/views.py
+++ tournament/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import TeamMatch, Team
+
 
 # Create your views here.
+class MatchSummary(ListView):
+    model = TeamMatch
+    template_name = "tournament/summary.html"
+    context_object_name = "summary"
+
+class MatchDetail(DetailView):
+    model = Team
+    template_name = "tournament/MatchDetail.html"
+    context_object_name = "matchdetails" 
