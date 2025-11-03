95d55a065cb1ffa6c09ae863af354fec7e76d437
e9sjadhav <somraj@enine.dev>
2025-11-03 12:54:50 +0530

    task 26 to 29 done. replaced methods with new fields, updated migrations, created signal

--- tournament/models.py
+++ tournament/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Team(models.Model):
     country = models.CharField(max_length=3, unique=True)
-    matches = models.IntegerField()
+    matches = models.IntegerField(default=0)
     won = models.BooleanField(default=False)
     lost = models.BooleanField(default=False)
 
